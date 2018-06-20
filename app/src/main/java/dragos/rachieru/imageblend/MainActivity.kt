b8e1357678c87f86cf3e9b6a54bf15f257a8c6ea
Rachieru Dragos <rachierudragos97@gmail.com>
2018-06-20 23:11:05 +0300

    Fixed landmark error

--- app/src/main/java/dragos/rachieru/imageblend/MainActivity.kt
+++ app/src/main/java/dragos/rachieru/imageblend/MainActivity.kt
@@ -1,12 +1,47 @@
 package dragos.rachieru.imageblend
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import dragos.rachieru.bmp_blender.BitmapBlender
+import io.reactivex.MaybeObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val blender = BitmapBlender(
+                this,
+                BitmapFactory.decodeResource(resources, R.drawable.left),
+                BitmapFactory.decodeResource(resources, R.drawable.right)
+        )
+        blender.blend()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : MaybeObserver<Bitmap> {
+                    override fun onSuccess(t: Bitmap) {
+                        image_result.setImageBitmap(t)
+                    }
+
+                    override fun onComplete() {
+                        Toast.makeText(this@MainActivity,"No face detected:(",Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
+                    }
+
+                })
     }
 }
